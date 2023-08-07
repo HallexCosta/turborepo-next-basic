e90038cb2fbdb74cbe392259e727a6593b65f412
HallexCosta <hallex.costa@hotmail.com>
2023-08-06 21:00:17 -0300

    feat: adjust paths

--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -1,6 +1,2 @@
-// component exports
 import "./styles.css";
-export * from "./Button";
-export * from "./Header";
-export * from "./SocialLink";
-export * from "./Container";
+export * from "./src";
---
+++ packages/ui/src/index.tsx
@@ -0,0 +1,5 @@
+// component exports
+export * from "./Container";
+
+export * from "./Header";
+export * from "./SocialLink";
