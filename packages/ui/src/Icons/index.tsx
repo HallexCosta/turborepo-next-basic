c011c823a63d9afa21e66d2241633b854bb7e5af
HallexCosta <hallex.costa@hotmail.com>
2024-02-23 12:34:42 -0300

    feat: add new package ui icons
    Email, Github, Whatsapp, Linkedin and Whatsapp

---
+++ packages/ui/src/Icons/index.tsx
@@ -0,0 +1,13 @@
+import { Email } from './Email'
+import { Github } from './Github'
+import { Linkedin } from './Linkedin'
+import { Location } from './Location'
+import { Whatsapp } from './Whatsapp'
+
+export default {
+    Email,
+    Github,
+    Linkedin,
+    Location,
+    Whatsapp
+}
