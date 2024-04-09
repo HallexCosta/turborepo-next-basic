f89278d3d88d2193f3a7e7307c1f0e8e6cd3b2f2
HallexCosta <hallex.costa@hotmail.com>
2024-04-09 18:36:14 -0300

    feat: add endpoint update person by username

---
+++ apps/hallexcosta/app/api/person/[username]/route.ts
@@ -0,0 +1,21 @@
+import {db} from "../../../../database";
+import {contacts, persons} from "../../../../database/schema";
+import {eq} from "drizzle-orm";
+
+export async function POST(request: Request, {
+    params
+}) {
+    const data = await request.json()
+
+    data.updatedAt = new Date()
+    const updated = await db.update(persons)
+        .set(data)
+        .where(eq(persons.username, params.username))
+
+    return new Response(JSON.stringify({
+        message: 'Personal data updated',
+        updated
+    }), {
+        status: 200
+    })
+}
