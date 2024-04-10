3baf52c2c503cdb4ff5361c3a92eeb1c2f2c5040
HallexCosta <hallex.costa@hotmail.com>
2024-04-10 01:10:01 -0300

    feat: add endpoint to delete achievement by id

---
+++ apps/hallexcosta/app/api/achievements/[workExperienceId]/[achievementId]/route.ts
@@ -0,0 +1,45 @@
+import {db} from "../../../../../database";
+import {achievements} from "../../../../../database/schema";
+import {eq} from "drizzle-orm";
+import {NextResponse} from "next/server";
+
+export async function DELETE(request: Request, {
+    params
+}) {
+    try {
+        console.log('DELETE')
+
+        const reference = {}
+        const [alreadyAchievement] = await db
+            .select()
+            .from(achievements)
+            .where(eq(achievements.id, params.achievementId))
+
+        if (!alreadyAchievement) {
+          throw new Error('Achievement already deleted')
+        }
+
+        await db.delete(achievements).where(eq(achievements.id, params.achievementId))
+        Object.assign(reference, {
+            workExperienceId: alreadyAchievement.workExperienceId,
+            achievementId: params.achievementId
+        })
+
+        return new Response(JSON.stringify({
+            message: 'Achievement data deleted',
+            reference,
+            type: 'delete'
+        }), {
+            status: 200
+        })
+    } catch (e) {
+        return new Response(JSON.stringify({
+            message: e.message,
+            stack: e.stackTrace
+        }), {
+            status: 400
+        })
+    }
+
+}
+
