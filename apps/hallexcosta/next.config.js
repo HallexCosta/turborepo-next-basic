1db12b3f4a9d90aafe074892ed175993c74d3bc7
HallexCosta <hallex.costa@hotmail.com>
2024-05-01 14:14:01 -0300

    chore: enable loggin fetches

--- apps/hallexcosta/next.config.js
+++ apps/hallexcosta/next.config.js
@@ -1,33 +1,49 @@
 module.exports = {
   reactStrictMode: true,
-  transpilePackages: ["ui"],
+  transpilePackages: ['ui'],
   optimizeFonts: false,
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "github.com",
-        port: "",
-        pathname: "/HallanCosta/be-a-savior/raw/main/docs/images/**",
+        protocol: 'https',
+        hostname: 'github.com',
+        port: '',
+        pathname: '/hallexcosta.png',
       },
       {
-        protocol: "https",
-        hostname: "github.com",
-        port: "",
-        pathname: "/HallexCosta/habits-tracker/blob/main/docs/images/web/**",
+        protocol: 'https',
+        hostname: 'github.com',
+        port: '',
+        pathname: '/HallanCosta/be-a-savior/raw/main/docs/images/**',
       },
       {
-        protocol: "https",
-        hostname: "github.com",
-        port: "",
-        pathname: "/HallexCosta/league-of-legends-login-page/blob/main/docs/images/light/**",
+        protocol: 'https',
+        hostname: 'github.com',
+        port: '',
+        pathname: '/HallexCosta/habits-tracker/blob/main/docs/images/web/**',
       },
       {
-        protocol: "https",
-        hostname: "raw.githubusercontent.com",
-        port: "",
-        pathname: "/HallexCosta/animes-united/main/assets/screens/anime-detail-watched-episodes.png"
+        protocol: 'https',
+        hostname: 'github.com',
+        port: '',
+        pathname: '/HallexCosta/league-of-legends-login-page/blob/main/docs/images/light/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/HallexCosta/animes-united/main/assets/screens/anime-detail-watched-episodes.png'
       },
     ],
-  }
+  },
+  // experimental: {
+  //   logging: {
+  //     level: 'verbose'
+  //   }
+  // }
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
 };
