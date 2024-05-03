4aa3a30e5e8ba71846007717da2932a8550b2d94
HallexCosta <hallex.costa@hotmail.com>
2024-05-03 10:42:10 -0300

    feat: set default api base url
    use https://hallexcosta.com/api as base url

--- apps/hallexcosta/app/fetches/get-person-by-username.ts
+++ apps/hallexcosta/app/fetches/get-person-by-username.ts
@@ -4,7 +4,9 @@ const getPersonByUsername = async (username: string = 'hallexcosta') => {
   console.log(process.env.NODE_ENV)
   console.log(process.env.API_BASE_URL ?? 'not value defined')
   console.log(process.env.NEXT_PUBLIC_API_BASE_URL ?? 'not value defined')
-  const response = await fetch(`${process.env.API_BASE_URL}/person/${username}`, {
+  const apiBaseUrl = process.env.API_BASE_URL ?? 'https://hallexcosta.com/api'
+  console.log(apiBaseUrl)
+  const response = await fetch(`${apiBaseUrl}/person/${username}`, {
     method: 'GET',
     next: {
       tags: ['get-resume-person']
